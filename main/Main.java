5596907a1559b35ae9bc0a38d1c06b6099021b07
Tatiana Pivovarova <titan4ic@gmail.com>
2020-04-18 00:05:22 +0300

    Add a check for any activity

--- main/Main.java
+++ main/Main.java
@@ -9,9 +9,11 @@ public class Main {
   public static void main(String[] args) throws IOException {
     Lexer lex = new Lexer();
     
+    System.out.write("start...\n");
     Token tok = lex.scan(); // temporary block is used to test earlier states
     do {
       System.out.write(tok.tag);
+      break;
       tok = lex.scan();
     } while (tok.tag != 0);
     
