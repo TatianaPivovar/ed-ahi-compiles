e544a2047e69385a2170a65082ba7c7d8e978f81
Tatiana Pivovarova <titan4ic@gmail.com>
2020-04-18 12:11:26 +0300

    Add concrete types to ArrayList

--- main/Main.java
+++ main/Main.java
@@ -10,16 +10,16 @@ public class Main {
     Lexer lex = new Lexer();
     
     System.out.println("start...");
-    ArrayList tokens = new ArrayList();
+    ArrayList<ArrayList<int> > tokens = new ArrayList<ArrayList<int> >();
     Token tok = lex.scan(); // temporary block is used to test earlier states
     do {
       if (tokens.size() < lex.line)
-        tokens.add(new ArrayList());
+        tokens.add(new ArrayList<int>());
       tokens.get(lex.line).add(tok.tag);
       tok = lex.scan();
     } while (tok.tag != (int)(char)(-1)); // EOL
     
-    for(ArrayList t: tokens)
+    for(ArrayList<int> t: tokens)
       System.out.println(t);
     
     //Parser parse = new Parser(lex);
