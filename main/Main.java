b7fed44aafbc49c85877c230cdcb4050a4eed15f
Tatiana Pivovarova <titan4ic@gmail.com>
2020-04-18 17:53:45 +0300

    Use enum Tag

--- main/Main.java
+++ main/Main.java
@@ -11,16 +11,16 @@ public class Main {
     Lexer lex = new Lexer();
     
     System.out.println("start...");
-    ArrayList<ArrayList<Integer> > tokens = new ArrayList<ArrayList<Integer> >();
+    ArrayList<ArrayList<Tag> > tokens = new ArrayList<ArrayList<Tag> >();
     Token tok = lex.scan(); // temporary block is used to test earlier states
     do {
       if (tokens.size() < lex.line)
         tokens.add(new ArrayList<Integer>());
       tokens.get(lex.line - 1).add(tok.tag);
       tok = lex.scan();
-    } while (tok.tag != (int)(char)(-1)); // EOL
+    } while (tok.tag != Tag.EOF);
     
-    for(ArrayList<Integer> t: tokens)
+    for(ArrayList<Tag> t: tokens)
       System.out.println(t);
     
     //Parser parse = new Parser(lex);
