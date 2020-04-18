177f6addc652009be41609a2b084606ba0b060fc
Tatiana Pivovarova <titan4ic@gmail.com>
2020-04-18 12:16:06 +0300

    Change int to Integer in templates

--- main/Main.java
+++ main/Main.java
@@ -11,16 +11,16 @@ public class Main {
     Lexer lex = new Lexer();
     
     System.out.println("start...");
-    ArrayList<ArrayList<int> > tokens = new ArrayList<ArrayList<int> >();
+    ArrayList<ArrayList<Integer> > tokens = new ArrayList<ArrayList<Integer> >();
     Token tok = lex.scan(); // temporary block is used to test earlier states
     do {
       if (tokens.size() < lex.line)
-        tokens.add(new ArrayList<int>());
+        tokens.add(new ArrayList<Integer>());
       tokens.get(lex.line).add(tok.tag);
       tok = lex.scan();
     } while (tok.tag != (int)(char)(-1)); // EOL
     
-    for(ArrayList<int> t: tokens)
+    for(ArrayList<Integer> t: tokens)
       System.out.println(t);
     
     //Parser parse = new Parser(lex);
