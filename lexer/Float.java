9bfed719fff532e93e55aa2305895f5f330ca3ee
Tatiana Pivovarova <titan4ic@gmail.com>
2020-05-02 13:32:36 +0300

    Create Float.java

---
+++ lexer/Float.java
@@ -0,0 +1,11 @@
+package lexer;
+public class Float extends Num {
+  private final float mantissa;
+  public Num(int int_val, float fraction) {
+    super(int_val);
+    mantissa = frac;
+  }
+  public int value() {
+    return characteristic + mantissa;
+  }
+}
