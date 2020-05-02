e588b1fc93bcc23209d229093bcc79f57d3b606f
Tatiana Pivovarova <titan4ic@gmail.com>
2020-05-02 13:34:54 +0300

    Fix constructor

--- lexer/Float.java
+++ lexer/Float.java
@@ -1,11 +1,11 @@
 package lexer;
 public class Float extends Num {
   private final float mantissa;
-  public Num(int int_val, float fraction) {
+  public Float(int int_val, float fraction) {
     super(int_val);
     mantissa = frac;
   }
-  public int value() {
+  public float value() {
     return characteristic + mantissa;
   }
 }
